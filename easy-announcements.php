c09f7c3ad71b3c74cbf1fd737311ae7ac90af3f9
Mason Miller <masonjmiller@hotmail.com>
2023-03-06 09:47:23 -0500

    Update 0.1.8
    - Updated ACF version to 6.0.7
    - Added check to see if ACF is already installed and not load local version.

--- easy-announcements.php
+++ easy-announcements.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Announcements
  * Plugin URI: https://www.phillywebteam.com/project/easy-announcements/
  * Description: Easily add announcements, banners, and popups to your WordPress site.
- * Version: 0.1.7
+ * Version: 0.1.8
  * Author: Philly Web Team
  * Author URI: https://www.phillywebteam.com
  * Text Domain: easy-announcements
@@ -14,22 +14,23 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 define( 'EASY_ANNOUNCEMENTS_ABSPATH', WP_PLUGIN_DIR . '/easy-announcements' );
 define( 'EASY_ANNOUNCEMENTS_PLUGIN_DIR', plugins_url( '/', __FILE__ ) );
-define( 'EASY_ANNOUNCEMENTS_VERSION', '0.1.7' );
+define( 'EASY_ANNOUNCEMENTS_VERSION', '0.1.8' );
 define( 'EASY_ANNOUNCEMENTS_ACF_PATH', EASY_ANNOUNCEMENTS_ABSPATH . '/includes/acf/' );
 define( 'EASY_ANNOUNCEMENTS_ACF_URL', EASY_ANNOUNCEMENTS_ABSPATH . '/includes/acf/' );
 
 require EASY_ANNOUNCEMENTS_ABSPATH . '/includes/functions.php';
 
-include_once( EASY_ANNOUNCEMENTS_ACF_PATH . 'acf.php' );
-
-add_filter( 'acf/settings/url', 'easy_announcements_acf_settings_url' );
-function easy_announcements_acf_settings_url( $url ) {
-    return EASY_ANNOUNCEMENTS_PLUGIN_DIR . '/includes/acf/';
-}
 if (
     !in_array( 'advanced-custom-fields/acf.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) &&
     !in_array( 'advanced-custom-fields-pro/acf.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) )
 ) {
+    include_once( EASY_ANNOUNCEMENTS_ACF_PATH . 'acf.php' );
+    
+    add_filter( 'acf/settings/url', 'easy_announcements_acf_settings_url' );
+    function easy_announcements_acf_settings_url( $url ) {
+        return EASY_ANNOUNCEMENTS_PLUGIN_DIR . '/includes/acf/';
+    }
+
     add_filter( 'acf/settings/show_admin', '__return_false' );
 }
 
